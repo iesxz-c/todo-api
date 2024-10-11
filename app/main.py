83d7a9c0506b055901963304d937310972b7a3a7
Akash <akash.krishnan.046@gmail.com>
2024-10-11 16:38:43 +0530

    allowing all origins

--- app/main.py
+++ app/main.py
@@ -14,12 +14,10 @@ from sqlmodel import Session
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-origins = [
-    "http://localhost:19006",  
-]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
